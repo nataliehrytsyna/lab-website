98c5a58262c37f05dc8f27f381da73bd67db3965
nataliehrytsyna <hrytsn@rpi.edu>
2026-04-18 10:39:03 -0400

    add mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
 
+if (menuBtn && navLinks) {
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+}
